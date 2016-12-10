d5c0f29d437065929f5c38f1a6f2ff46e40fad20
lizhong43403 <lizhong43403@163.com>
2016-12-10 21:24:14 +0800

    yii 环境设置 - 新增 本地配置信息

--- README.md
+++ README.md
@@ -3,9 +3,10 @@ Team code
 
 http://ahjwen.oschina.io/soho/
 
-一个为开发者而开发的神奇的网站。<a href="mailto:lizhong43403@sina.com">**意见反馈邮箱**</a>
+一个为开发者而开发的神奇的网站。<a href="mailto:lizhong43403@sina.com?cc=ahjwen@163.com&subject=意见反馈&body=亲爱的开发者，你好！">**意见反馈**</a>
 
 ------
 - [yii框架文档](http://www.yiiframework.com/doc/), [yii下载地址](http://www.yiiframework.com/download/)
 - **当前框架还未将 vendor 插件加入到代码库中**
+- [a:mailto 参考链接](http://www.haorooms.com/post/mailto_link_html)
 
--- environments/index.php
+++ environments/index.php
@@ -29,6 +29,23 @@
  * ```
  */
 return [
+    'local' => [
+        'path' => 'local',
+        'setWritable' => [
+            'backend/runtime',
+            'backend/web/assets',
+            'frontend/runtime',
+            'frontend/web/assets',
+        ],
+        'setExecutable' => [
+            'yii',
+            'yii_test',
+        ],
+        'setCookieValidationKey' => [
+            'backend/config/main-local.php',
+            'frontend/config/main-local.php',
+        ],
+    ],
     'Development' => [
         'path' => 'dev',
         'setWritable' => [
